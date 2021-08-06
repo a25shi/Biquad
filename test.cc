a086694ead8cb6c9b77d3d2638c66bde405287a5
Preesha Ruparelia <50782597+preeshar27@users.noreply.github.com>
2021-08-06 18:49:50 -0400

    fixed formatting of the next block preview

--- test.cc
+++ test.cc
@@ -1,19 +1,22 @@
 #include <iomanip>
 #include "board.h"
-#include "RAIILevel.h"
-
+#include "level0.h"
+#include "level1.h"
+#include "level2.h"
+#include "level3.h"
+#include "level4.h"
 using namespace std;
 
-void blockGen(Board &b, generation *l, bool call) {
+void blockGen(Board &b, generation* l, bool call) {
     char block = l->genBlock();
-    string s(1, block);
+    string s (1, block);
     if (call) {
         b.next();
     }
     b.setNext(s);
 }
 
-ostream &operator<<(ostream &out, Board &b) {
+ostream& operator<<( ostream & out, Board &b) {
     out << "Level:";
     out << right;
     out << setw(5) << b.getLevel() << endl;
@@ -39,46 +42,64 @@ ostream &operator<<(ostream &out, Board &b) {
 
 int main(int argc, char *argv[]) {
     Board b;
-    RAIILevel levelvector{"sequence1.txt"};
-    shared_ptr<generation> l = levelvector.getLevel(3);
-    b.setLevel(3);
-    blockGen(b, l.get(), false);
-    blockGen(b, l.get(), true);
-    cout << b << endl;
     string cmd;
-    while (cin >> cmd) {
-        if (cmd == "move") {
-            char dir;
-            cin >> dir;
-            if (dir == 'r') {
-                b.move("r");
-                cout << b << endl;
-            } else if (dir == 'l') {
-                b.move("l");
-                cout << b << endl;
-            } else if (dir == 'd') {
-                b.move("d");
+    if (argc == 1) {
+        level1 l;
+        blockGen(b, &l, false);
+        blockGen(b, &l, true);
+        cout << b << endl;
+        while (cin >> cmd) {
+            if (cmd == "move") {
+                char dir;
+                cin >> dir;
+                if (dir == 'r') {
+                    b.move("r");
+                    cout << b << endl;
+                } else if (dir == 'l') {
+                    b.move("l");
+                    cout << b << endl;
+                } else if (dir == 'd') {
+                    b.move("d");
+                    cout << b << endl;
+                } else {
+                    cout << "Invalid Direction!" << endl;
+                }
+            } else if (cmd == "drop") {
+                b.drop();
+                blockGen(b, &l, true);
                 cout << b << endl;
             } else {
-                cout << "Invalid Direction!" << endl;
+                cout << "Invalid Argument!" << endl;
             }
-        } else if (cmd == "drop") {
-            b.drop();
-            blockGen(b, l.get(), true);
-            cout << b << endl;
-        } else if (cmd == "level") {
-            int level;
-            cin >> level;
-            try {
-                l = levelvector.getLevel(level);
-                b.setLevel(level);
+        }
+    } else {
+        string blockToMake = string(1, argv[1][0]);
+        b.setNext(blockToMake);
+        b.next();
+        cout << b << endl;
+        while (cin >> cmd) {
+            if (cmd == "move") {
+                char dir;
+                cin >> dir;
+                if (dir == 'r') {
+                    b.move("r");
+                    cout << b << endl;
+                } else if (dir == 'l') {
+                    b.move("l");
+                    cout << b << endl;
+                } else if (dir == 'd') {
+                    b.move("d");
+                    cout << b << endl;
+                } else {
+                    cout << "Invalid Direction!" << endl;
+                }
+            } else if (cmd == "drop") {
+                b.next();
+                b.setNext(blockToMake);
                 cout << b << endl;
+            } else {
+                cout << "Invalid Argument!" << endl;
             }
-            catch (error) {
-                cerr << "Error: Level not in bounds." << endl;
-            }
-        } else {
-            cout << "Invalid Argument!" << endl;
         }
     }
 }
