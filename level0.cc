605a90df648e01e8502e55c4c264fa3e6971e5a8
Preesha Ruparelia <50782597+preeshar27@users.noreply.github.com>
2021-08-06 18:37:48 -0400

    fixed syntax error

--- level0.cc
+++ level0.cc
@@ -6,7 +6,7 @@ level0::level0(std::string file) : generation{0}, file{file}{
 
 char level0::genBlock() {
     if (index >= blocks.size()) {
-        index == 0;
+        index = 0;
     }
     return blocks[index++];
 }
