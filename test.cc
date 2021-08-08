fd35e2ccccbd3e2a9fdeb37ea008f73189420108
allen <allen_shi2002@hotmail.com>
2021-08-07 22:14:57 -0400

    fixed level not showing on display

--- test.cc
+++ test.cc
@@ -41,6 +41,8 @@ int main(int argc, char *argv[]) {
     }
     Board p1;
     Board p2;
+    p1.setLevel(level);
+    p2.setLevel(level);
     controller ct{&p1, &p2};
     ct.play(in1, in2, level);
 }
