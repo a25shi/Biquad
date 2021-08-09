e98617bef387490f794603559dd069c9407e24cb
Denise Albeanu <63042788+denisealbeanu@users.noreply.github.com>
2021-08-08 20:43:10 -0400

    Delete blockJ.cc

--- blockJ.cc
+++
@@ -1,4 +0,0 @@
-#include "blockJ.h"
-
-BlockJ::BlockJ(Cell *component, vector<vector<int>> coord,
-               int curLevel, vector<vector<int>> rotation, int stage) : Block{component, "J", coord, curLevel, rotation, stage} {}
