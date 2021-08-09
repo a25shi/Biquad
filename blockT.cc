78b16d770fa5cabca012d61896cf18ecbc9ae920
Denise Albeanu <63042788+denisealbeanu@users.noreply.github.com>
2021-08-08 20:44:40 -0400

    Delete blockT.cc

--- blockT.cc
+++
@@ -1,4 +0,0 @@
-#include "blockT.h"
-
-BlockT::BlockT(Cell *component, vector<vector<int>> coord,
-               int curLevel, vector<vector<int>> rotation, int stage) : Block{component, "T", coord, curLevel, rotation, stage} {}
