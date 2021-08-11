071aa7dc8fe41b001a40ad4ffc37c78b88a8e3cd
Preesha Ruparelia <50782597+preeshar27@users.noreply.github.com>
2021-08-10 22:45:38 -0400

    Delete blockO.cc

--- blockO.cc
+++
@@ -1,4 +0,0 @@
-#include "blockO.h"
-
-BlockO::BlockO(Cell *component, vector<vector<int>> coord,
-               int curLevel, vector<vector<int>> rotation, int stage) : Block{component, "O", coord, curLevel, rotation, stage} {}
