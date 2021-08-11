84dd022972a54891b58fa4f0257a891fbca4ce24
Preesha Ruparelia <50782597+preeshar27@users.noreply.github.com>
2021-08-10 22:45:02 -0400

    Delete blockI.cc

--- blockI.cc
+++
@@ -1,4 +0,0 @@
-#include "blockI.h"
-
-BlockI::BlockI(Cell *component, vector<vector<int>> coord,
-               int curLevel, vector<vector<int>> rotation, int stage) : Block{component, "I", coord, curLevel, rotation, stage} {}
