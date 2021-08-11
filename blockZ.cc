a52c27f6fda5c2ba2884aeb66972cc8eac80a847
Preesha Ruparelia <50782597+preeshar27@users.noreply.github.com>
2021-08-10 22:46:20 -0400

    Delete blockZ.cc

--- blockZ.cc
+++
@@ -1,4 +0,0 @@
-#include "blockZ.h"
-
-BlockZ::BlockZ(Cell *component, vector<vector<int>> coord,
-               int curLevel, vector<vector<int>> rotation, int stage) : Block{component, "Z", coord, curLevel, rotation, stage} {}
